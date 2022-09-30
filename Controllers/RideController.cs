01d9e6a245517bb88aaa0cd20f8ec7278c57b532
v-rowjee <vedrowjee2001@gmail.com>
2022-09-30 16:45:33 +0400

    Share Ride

---
+++ Controllers/RideController.cs
@@ -0,0 +1,55 @@
+﻿using JEDI_Carpool.DAL.Common;
+using JEDI_Carpool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JEDI_Carpool.Controllers
+{
+    public class RideController : Controller
+    {
+        // GET: Ride
+        public ActionResult Index()
+        {
+            var loggeduser = Session["CurrentUser"] as LoginViewModel;
+            var homeView = View();
+
+            if (loggeduser != null)
+            {
+                var data = AccountBL.GetAccountDetails(loggeduser);
+                ViewBag.Account = data;
+
+                homeView.MasterName = "~/Views/Shared/_Layout.cshtml";
+            }
+            else
+            {
+                homeView.MasterName = "~/Views/Shared/_GuestLayout.cshtml";
+            }
+
+            return homeView;
+        }
+
+        public ViewResult Share()
+        {
+            var loggeduser = Session["CurrentUser"] as LoginViewModel;
+            var homeView = View();
+
+            if (loggeduser != null)
+            {
+                var data = AccountBL.GetAccountDetails(loggeduser);
+                ViewBag.Account = data;
+
+                homeView.MasterName = "~/Views/Shared/_Layout.cshtml";
+            }
+            else
+            {
+                homeView.MasterName = "~/Views/Shared/_GuestLayout.cshtml";
+            }
+
+            return homeView;
+        }
+
+    }
+}
