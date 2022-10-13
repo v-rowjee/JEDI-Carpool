184d3375a7afa9152ddf666d4d61e93aa250a5a7
v-rowjee <vedrowjee2001@gmail.com>
2022-10-13 23:01:52 +0400

    share ride UI changed to wizard

--- Scripts/site.js
+++ Scripts/site.js
@@ -17,3 +17,21 @@
         }, false)
     })
 })()
+
+// https://codepen.io/adethis/pen/BaWMLKO
+$(document).ready(function () {
+    // Action next
+    $('.btn-next').on('click', function () {
+        // Get value from data-to in button next
+        const n = $(this).attr('data-to');
+        // Action trigger click for tag a with id in value n
+        $(n).trigger('click');
+    });
+    // Action back
+    $('.btn-prev').on('click', function () {
+        // Get value from data-to in button prev
+        const n = $(this).attr('data-to');
+        // Action trigger click for tag a with id in value n
+        $(n).trigger('click');
+    });
+});
