d88d3f01d75262c126897bc509832654361ecda0
v-rowjee <vedrowjee2001@gmail.com>
2022-10-21 21:37:23 +0400

    publishing

--- DAL/Common/DAL.cs
+++ DAL/Common/DAL.cs
@@ -9,6 +9,7 @@ namespace JEDI_Carpool.DAL.Common
     public class DAL
     {
         public const string connectionString = @"server=L-PC197T2Z;database=CarpoolManagement;uid=wbpoc;pwd=sql@tfs2008";
+        //public const string connectionString = @"Data Source=L-PC197T2Z;Initial Catalog=CarpoolManagement;Persist Security Info=True;User ID=wbpoc;Password=sql@tfs2008";
 
         public SqlConnection Connection;
 
